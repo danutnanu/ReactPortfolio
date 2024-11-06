a24ce47172dec1dbaacbc3e0ae019ea518930044
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-11-06 16:59:37 +0000

    Add Navbar to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Navbar />
       <div>
-        <p>Hello World</p>
+        <button className="btn btn-primary bg-dark text-white">Primary Button</button>
       </div>
     </>
   )
