d8627b86d2d81916f07c267c292efc9f1e4a0ae2
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-11-07 10:27:03 +0000

    Integrate global theme toggle functionality in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navigation from './components/Navbar';
 import Home from './components/Home';
@@ -6,16 +7,22 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleTheme = () => setDarkMode(!darkMode);
+
   return (
-    <Router>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
+    <div className={darkMode ? 'dark-mode' : ''}>
+      <Router>
+        <Navigation toggleTheme={toggleTheme} darkMode={darkMode} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
