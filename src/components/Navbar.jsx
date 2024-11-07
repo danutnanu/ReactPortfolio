2e2ea2b1f74ad2370e57788982b3ccbb60dba3e0
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-11-07 10:26:38 +0000

    Implement theme toggle in navbar with icons and switch

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,26 +3,30 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 
-function Navigation() {
+function Navigation({ toggleTheme, darkMode }) {
   const [expanded, setExpanded] = useState(false);
 
   const handleToggle = () => setExpanded(!expanded);
   const handleClose = () => setExpanded(false);
 
   return (
-    <Navbar expand="lg" className="bg-body-tertiary w-100 fixed-top" expanded={expanded}>
+    <Navbar expand="lg" className={`bg-body-tertiary w-100 fixed-top ${darkMode ? 'bg-dark' : 'bg-light'}`} expanded={expanded}>
       <Container>
         <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleToggle} />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mx-auto gap-5">
+          <Nav className="mx-auto gap-5 fs-4">
             <Nav.Link as={Link} to="/" onClick={handleClose}>Home</Nav.Link>
             <Nav.Link as={Link} to="/about" onClick={handleClose}>About</Nav.Link>
             <Nav.Link as={Link} to="/projects" onClick={handleClose}>Projects</Nav.Link>
             <Nav.Link as={Link} to="/contact" onClick={handleClose}>Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
-        <Navbar.Brand as={Link} to="/" onClick={handleClose}>Dark/Light Mode</Navbar.Brand>
+        <button onClick={toggleTheme} className="btn btn-outline-secondary">
+          <FontAwesomeIcon icon={darkMode ? faSun : faMoon} />
+        </button>
       </Container>
     </Navbar>
   );
