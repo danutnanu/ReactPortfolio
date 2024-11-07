2d9bd4fcbfe1e6bf12b51eb61fd91b5804a0115c
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-11-07 07:45:22 +0000

    Add links to Home, About, Projects, and Contact in the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,25 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
 
-function BasicExample() {
+function Navigation() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary W-100">
+    <Navbar expand="lg" className="bg-body-tertiary w-100 fixed-top">
       <Container>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+          <Nav className="mx-auto gap-5">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+            <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">Dark/Light Mode</Navbar.Brand>
       </Container>
     </Navbar>
   );
 }
 
-export default BasicExample;
+export default Navigation;
