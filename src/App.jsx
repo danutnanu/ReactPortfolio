d58de47e7cef3073595fc21c154b73a15473df25
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-11-09 09:36:51 +0000

    import index.css in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import './index.css';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
