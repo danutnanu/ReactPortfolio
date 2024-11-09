d16459dca378072d1f99334f115574deb6631653
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-11-09 17:30:45 +0000

    feat: add text and image to Home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
-import Type from './Type'; 
+import Type from './Type';
 
 function Home() {
   return (
     <div className="d-flex justify-content-center align-items-center vh-100 text-center">
       <div>
-        <h1>Hi, I'm:</h1>
+        <h1>
+          Hi, there!
+          <span className="wave" role="img" aria-labelledby="wave">
+            👋🏻
+          </span>
+        </h1>
+        <h1>
+          I'm Danut Nanu
+        </h1>
         <h1>
           <Type />
         </h1>
       </div>
+      <img src="/profile.png" alt="Profile" style={{ maxWidth: '230px', height: 'auto', marginLeft: '40px', borderRadius: '5%' }} />
     </div>
   );
 }
