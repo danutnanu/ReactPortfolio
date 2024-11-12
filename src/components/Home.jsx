ef4c612a359ed28039556e421b4441069d4640c1
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-11-12 07:52:58 +0000

    Updated Home component to use image icons for email, LinkedIn, and GitHub.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,23 +3,35 @@ import Type from './Type';
 
 function Home() {
   return (
-    <div className="d-flex justify-content-center align-items-center vh-100 text-center">
-      <div>
-        <h1>
-          Hi, there!
-          <span className="wave" role="img" aria-labelledby="wave">
-            👋🏻
-          </span>
-        </h1>
-        <h1>
-          I'm Danut Nanu
-        </h1>
-        <h1>
-          <Type />
-        </h1>
+    <>
+      <div className="d-flex align-items-center m-5 vh-100">
+        <div className='ms-5 ps-5'>
+          <h2>
+            Hi, there!
+            <span className="wave" role="img" aria-labelledby="wave">
+              👋🏻
+            </span>
+          </h2>
+          <h1 style={{ fontSize: '5rem' }}>
+            Danut Nanu
+          </h1>
+          <h2>
+            <Type />
+          </h2>
+          <div className="d-flex mt-5">
+            <a href="mailto:danutnanu@icloud.com" target="_blank" className="me-3 text-decoration-none text-dark" aria-label="Email">
+              <img src="/email.png" alt="Email" style={{ width: '40px', height: '40px' }} />
+            </a>
+            <a href="https://www.linkedin.com/in/danut-nanu-7474b4267/" target="_blank" className="me-3 text-decoration-none text-dark" aria-label="LinkedIn">
+              <img src="/linkedin2.png" alt="LinkedIn" style={{ width: '40px', height: '40px' }} />
+            </a>
+            <a href="https://github.com/danutnanu" target="_blank" className="text-decoration-none text-dark" aria-label="GitHub">
+              <img src="/github2.png" alt="GitHub" style={{ width: '40px', height: '40px' }} />
+            </a>
+          </div>
+        </div>
       </div>
-      <img src="/profile.png" alt="Profile" style={{ maxWidth: '230px', height: 'auto', marginLeft: '40px', borderRadius: '5%' }} />
-    </div>
+    </>
   );
 }
 
