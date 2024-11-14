7baf8799618c65b23bdbae632f3d49c1c73ee25c
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-11-14 18:02:35 +0000

    Introduced a new button for View Project and GitHub.

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function ProjectItem({ img, description, link }) {
+function ProjectItem({ img, description, link, githubLink }) {
   return (
     <div className="position-relative project-item-container">
       <Link className="project-item-link" to={link}>
         <img className="img-fluid project-item-img" alt={description} src={img} />
         <div className="overlay">
           <h5 className="project-item-text text-center">{description}</h5>
-          <button className="btn custom-button mt-2">View Project</button>
+          <div className="button-container">
+            <Link to={link} className="btn btn-primary mx-2">View Project</Link>
+            <Link to={githubLink} className="btn btn-secondary mx-2">GitHub</Link>
+          </div>
         </div>
       </Link>
     </div>
