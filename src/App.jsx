89bdffc9447addc2d13908e273350b8aa5742df4
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-11-16 21:56:16 +0000

    Refactor: Move Contact component styles to Contact.css and clean up index.css

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navigation from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Projects from './components/Projects';
-import Contact from './components/Contact';
+import Contact from './components/Contact/Contact';
 import './index.css';
 
 function App() {
