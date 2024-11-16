d6e51907153017fc3f43d9b28e0a5ac12fcc600e
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-11-16 21:48:54 +0000

    Refactor: Move About component styles to About.css and clean up index.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navigation from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
-import About from './components/About';
+import About from './components/About/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import './index.css';
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import './About.css'; 
 
 function About() {
   return (
-    <div className="container-fluid mt-5 pt-5 px-5 vh-100">
+    <Container fluid className="mt-5 pt-5 px-5 vh-100">
       <h1 className="text-center my-2">About Me</h1>
-      <div className="border-container my-5 ps-5 pt-5" style={{ display: 'flex', borderRadius: '8px', overflow: 'hidden', width: '100%' }}>
-        <div className="image-section ms-4" style={{ flex: '1', display: 'flex', alignItems: 'center', padding: '0 70px' }}>
+
+      {/* First Section */}
+      <Container className="border-container my-5" style={{ display: 'flex', borderRadius: '8px', overflow: 'hidden', width: '100%' }}>
+        <div className="image-section" style={{ flex: '1', display: 'flex', alignItems: 'center', padding: '0 20px' }}>
           <img src="/profile.png" alt="Description of image" className="img-fluid rounded border border-dark" style={{ height: '100%', maxHeight: '400px', width: 'auto' }} />
         </div>
-        <div className="text-section me-4 pe-5" style={{ flex: '1', padding: '0 20px' }}>
+        <div className="text-section" style={{ flex: '1', padding: '0 20px' }}>
           <div className="terminal">
             <div className="terminal-header">
               <span className="circle red"></span>
@@ -17,13 +21,15 @@ function About() {
             </div>
             <div className="terminal-body fs-5">
               <p><span className="command">danutnanu $</span> <span className="lighter">cat aboutdanut</span></p>
-              <p><span className="command">aboutdanut (main) $</span> <span className="lighter">Hello! I'm Danut Nanu. A devoted family man known for my friendly nature and a deep passion for sports. I find immense joy in putting on my running shoes, participating in marathons, and pushing my limits. Beyond my active lifestyle, I'm a motivated Junior Frontend Developer with a solid foundation in web development, particularly in technologies like React, JavaScript, Bootstrap, HTML, and CSS. I am excited about the opportunity to join an innovative team of like-minded individuals. Let's get in touch and look at potential collaborations! </span></p>
+              <p><span className="command">aboutdanut (main) $</span> <span className="lighter">Hello! I'm Danut Nanu. A devoted family man known for my friendly nature and a deep passion for sports. I find immense joy in putting on my running shoes, participating in marathons, and pushing my limits. Beyond my active lifestyle, I'm a motivated Junior Frontend Developer with a solid foundation in web development, particularly in technologies like React, JavaScript, Bootstrap, HTML, and CSS. I am excited about the opportunity to join an innovative team of like-minded individuals. Let's get in touch and look at potential collaborations!</span></p>
             </div>
           </div>
         </div>
-      </div>
-      <div className="border-container" style={{ display: 'flex', borderRadius: '8px', overflow: 'hidden', width: '100%' }}>
-        <div className="text-section m-5 px-5">
+      </Container>
+
+      {/* Second Section */}
+      <Container className="border-container my-5" style={{ display: 'flex', borderRadius: '8px', overflow: 'hidden', width: '100%' }}>
+        <div className="text-section" style={{ flex: '1', padding: '0 20px' }}>
           <div className="terminal">
             <div className="terminal-header">
               <span className="circle red"></span>
@@ -52,15 +58,17 @@ function About() {
             </div>
           </div>
         </div>
-        <div className="image-section m-5 px-5" style={{ flex: '1', display: 'flex', alignItems: 'center' }}>
+        <div className="image-section" style={{ flex: '1', display: 'flex', alignItems: 'center', padding: '0 20px' }}>
           <img src="/profile.png" alt="Description of image" className="img-fluid rounded border border-dark" style={{ height: '100%', maxHeight: '400px', width: 'auto' }} />
         </div>
-      </div>
-      <div className="border-container my-5 mx-0" style={{ display: 'flex', borderRadius: '8px', overflow: 'hidden', width: '100%' }}>
-        <div className="image-section m-4" style={{ flex: '1', display: 'flex', alignItems: 'center', padding: '0 80px' }}>
+      </Container>
+
+      {/* Third Section */}
+      <Container className="border-container my-5" style={{ display: 'flex', borderRadius: '8px', overflow: 'hidden', width: '100%' }}>
+        <div className="image-section" style={{ flex: '1', display: 'flex', alignItems: 'center', padding: '0 20px' }}>
           <img src="/profile.png" alt="Description of image" className="img-fluid rounded border border-dark" style={{ height: '100%', maxHeight: '400px', width: 'auto' }} />
         </div>
-        <div className="text-section m-4" style={{ flex: '1', padding: '0 20px' }}>
+        <div className="text-section" style={{ flex: '1', padding: '0 20px' }}>
           <div className="terminal">
             <div className="terminal-header">
               <span className="circle red"></span>
@@ -72,22 +80,19 @@ function About() {
               <p><span className="command">certificate (main) $</span> <span className="lighter">ls</span></p>
               <p className="section-title">FreeCodeCamp Frontend Certificates</p>
               <ul className="skills">
-                <li><a href="./public/responsive_web_design.png" title="Responsive Web Design Certificate"
-                  target="_blank">Responsive Web Design (html, css)</a></li>
-                <li><a href="./public/javascript.png" title="JavaScript Algorithms and Data
-                Structures Certificate" target="_blank">JavaScript Algorithms and Data Structures (JavaScript)</a></li>
-                <li><a href="./public/frontend-libraries.png" title="Front End Development Libraries Certificate"
-                  target="_blank">Front End Development Libraries (React, Bootstrap, jQuery)</a></li>
+                <li><a href="./public/responsive_web_design.png" title="Responsive Web Design Certificate" target="_blank">Responsive Web Design (html, css)</a></li>
+                <li><a href="./public/javascript.png" title="JavaScript Algorithms and Data Structures Certificate" target="_blank">JavaScript Algorithms and Data Structures (JavaScript)</a></li>
+                <li><a href="./public/frontend-libraries.png" title="Front End Development Libraries Certificate" target="_blank">Front End Development Libraries (React, Bootstrap, jQuery)</a></li>
                 <li><a href="./public/data-visualization.png" title="Data Visualization Certificate" target="_blank">Data Visualization (D3.js)</a></li>
                 <li><a href="./public/backend.png" title="Back End Development and APIs Certificate" target="_blank">Back End Development and APIs (Node, Express, MongoDB)</a></li>
-                <li><a href="./public/backend.png" title="Back End Development and APIs Certificate" target="_blank">Relational Database (Bash and SQL)</a></li>
+                <li><a href="./public/backend.png" title="Relational Database Certificate" target="_blank">Relational Database (Bash and SQL)</a></li>
                 <li><a href="./public/quality-assurance.png" title="Quality Assurance Certificate" target="_blank">Quality Assurance (Chai, Mocha)</a></li>
               </ul>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      </Container>
+    </Container>
   );
 }
 
