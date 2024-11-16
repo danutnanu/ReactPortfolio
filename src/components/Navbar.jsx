b3b6cc689e81c5a324f3a6c27dce2f1b3e3f82a6
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-11-16 17:26:22 +0000

    Update Navbar.jsx: Center logo on small devices, adjust margins for hamburger menu and dark/light button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,19 +19,25 @@ function Navigation({ toggleTheme, darkMode }) {
       expanded={expanded}
     >
       <Container>
-      <span className="badge text-bg-light text-wrap fst-italic p-1 border border-warning">Portfolio</span>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleToggle} />
+        <div className="d-flex justify-content-between align-items-center w-100">
+          <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleToggle} className="order-1 ms-2 me-2" />
+          <span className="badge text-bg-light p-1 ms-2 border border-warning order-2 d-lg-none">Portfolio</span>
+          <span className="badge text-bg-light p-1 border border-warning order-0 d-none d-lg-inline">Portfolio</span>
+          <button onClick={toggleTheme} className="btn btn-outline-secondary order-3 ms-4 me-2 d-lg-none">
+            {darkMode ? '🌔' : '🌘'} 
+          </button>
+        </div>
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto gap-5 fs-4">
+          <Nav className="ms-auto gap-5 fs-4 align-items-center">
             <Nav.Link as={Link} to="/" onClick={handleClose} className="nav-link">home</Nav.Link>
             <Nav.Link as={Link} to="/about" onClick={handleClose} className="nav-link">about</Nav.Link>
             <Nav.Link as={Link} to="/projects" onClick={handleClose} className="nav-link">projects</Nav.Link>
             <Nav.Link as={Link} to="/contact" onClick={handleClose} className="nav-link">contact</Nav.Link>
+            <button onClick={toggleTheme} className="btn btn-outline-secondary d-none d-lg-block">
+              {darkMode ? '🌔' : '🌘'} 
+            </button>
           </Nav>
         </Navbar.Collapse>
-        <button onClick={toggleTheme} className="ms-5 btn btn-outline-secondary">
-          {darkMode ? '🌔' : '🌘'} 
-        </button>
       </Container>
     </Navbar>
   );
