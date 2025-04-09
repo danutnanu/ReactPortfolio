ebe5fc6de903fb3e12db6ab946c57f5dde925980
danut nanu <125583127+danutnanu@users.noreply.github.com>
2025-04-09 12:19:49 +0100

    Rearrange social media links in Home component.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -23,11 +23,11 @@ function Home() {
             <a href="mailto:danutnanu@icloud.com" target="_blank" className="me-3 text-decoration-none text-dark" aria-label="Email">
               <img src="/email.png" alt="Email" style={{ width: '40px', height: '40px' }} />
             </a>
-            <a href="https://www.linkedin.com/in/danut-nanu-7474b4267/" target="_blank" className="me-3 text-decoration-none text-dark" aria-label="LinkedIn">
-              <img src="/linkedin2.png" alt="LinkedIn" style={{ width: '40px', height: '40px' }} />
-            </a>
-            <a href="https://github.com/danutnanu" target="_blank" className="text-decoration-none text-dark" aria-label="GitHub">
+            <a href="https://github.com/danutnanu" target="_blank" className="me-3 text-decoration-none text-dark" aria-label="GitHub">
               <img src="/github2.png" alt="GitHub" style={{ width: '40px', height: '40px' }} />
+            </a>
+            <a href="https://www.linkedin.com/in/danut-nanu-7474b4267/" target="_blank" className="text-decoration-none text-dark" aria-label="LinkedIn">
+              <img src="/linkedin2.png" alt="LinkedIn" style={{ width: '40px', height: '40px' }} />
             </a>
           </div>
         </div>
